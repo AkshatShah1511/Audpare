6f298012e70d3461c35f51ad24969c04ef8c0ac7
Akshat Shah <shahakshat1511@gmail.com>
2025-02-08 22:30:09 +0530

    Initial Commit

---
+++ Audpare/AudpareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudpareApp.swift
+//  Audpare
+//
+//  Created by Akshat on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AudpareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audpare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Audpare
+//
+//  Created by Akshat on 08/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
